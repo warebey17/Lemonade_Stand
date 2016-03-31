23e21c40f8207ec73f8eaf1145c8f33866ed7267
Quentin Ware-Bey <Quentin Ware-Bey>
2016-03-31 13:20:06 -0500

    Lemonade stand corrections

--- ConsoleApplication4/game.cs
+++ ConsoleApplication4/game.cs
@@ -35,7 +35,13 @@ namespace ConsoleApplication4
             if ( decisionToPlay == 1)
 
         {
-            Console.WriteLine("Great, let's get started \n");
+                Console.WriteLine("Great, let's get started!!!!!!!! \n");
+                Console.WriteLine("You have 7 days to make as much money as possible!");
+                Console.WriteLine("You’ll have complete control over your business,");
+                Console.WriteLine("including pricing, inventory control, and purchasing supplies.");
+                Console.WriteLine("Buy your ingredients and start selling! \n");
+                Console.WriteLine("Click enter/return to continue");
+                Console.ReadLine();
         }
         else
         {
@@ -43,6 +49,10 @@ namespace ConsoleApplication4
             Thread.Sleep(4000);
             Environment.Exit(0);
           }
+
+
+
+
             {
                 weather Weather = new weather();
 
@@ -60,21 +70,21 @@ namespace ConsoleApplication4
             Console.Write("Please enter your LAST NAME: \n");
             string lastName = Console.ReadLine();
 
-            Console.Write("Place your order for ICE: \n");
+            Console.Write("Enter your option for ICE: (a. 100 @ .86, b. 250 @ 2.10, c. 500 @ 3.50)\n");
             string ice = Console.ReadLine();
 
-            Console.Write("Place your order for LEMONS: \n");
+            Console.Write("Enter your option for LEMONS: (a. 10 @ .97, b. 30 @ 2.48, c. 75 @ 4.32)\n");
             string lemons = Console.ReadLine();
 
-            Console.Write("Place your order for SUGAR: \n");
-            var sugar = Console.ReadLine();
+            Console.Write("Enter your option for SUGAR: (a. 8 @ .73, b. 20 @ 1.71, c. 48 @ 3.33)\n");
+            string sugar = Console.ReadLine();
 
-            Console.Write("Place your order for CUPS: \n");
-            var cups = Console.ReadLine();
+            Console.Write("Enter your option for CUPS: (a. 25 @ .78, b. 50 @ 1.53, c. 100 @ 2.84)\n");
+            string cups = Console.ReadLine();
 
             var Player = new player(firstName, lastName);
 
-            Console.WriteLine("Customer {0} {1}", Player.firstName, Player.lastName);
+            Console.WriteLine("Hello {0} {1}", Player.firstName, Player.lastName);
 
             //Console.WriteLine("You bought")
             //Console.WriteLine("You bought")
